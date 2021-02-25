f7bdabd562718382d655d289b5591f3fd2341645
Lucas Paiva <lukspaiva95@gmail.com>
2021-02-25 00:00:57 -0300

    saving tickets as json

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,35 @@
 const state = require("./state.js");
 
-function run() {}
+function run() {
+  const content = state.load();
+
+  if (!content.responses) return null;
+
+  for (const responseIndex of content.responses) {
+    const responseContent = state.loadIndividual(responseIndex);
+
+    const responseTickets = responseContent.tickets.map(
+      ({ id, subject, description, requester_id, generated_timestamp }) => {
+        const tickerUser = responseContent.users.find(
+          (user) => user.id === requester_id
+        );
+
+        const date = new Date(generated_timestamp * 1000).toLocaleDateString(
+          "pt-br"
+        );
+
+        return {
+          id,
+          subject,
+          description,
+          name: tickerUser?.name,
+          email: tickerUser?.email,
+          date,
+        };
+      }
+    );
+    state.saveTickets(responseTickets, responseIndex);
+  }
+}
 
 module.exports = run;
--- src/state.js
+++ src/state.js
@@ -6,19 +6,36 @@ function save(content) {
   return fs.writeFileSync(contentFilePath, contentString);
 }
 
-function saveIndividual(content, fileName) {
-  const contentString = JSON.stringify(content);
-  return fs.writeFileSync(`./tmp/responses/${fileName}.json`, contentString);
-}
-
 function load() {
   const fileBuffer = fs.readFileSync(contentFilePath, "utf-8");
   const contentJson = JSON.parse(fileBuffer);
   return contentJson;
 }
 
+function saveIndividual(content, fileName) {
+  const contentString = JSON.stringify(content);
+  return fs.writeFileSync(`./tmp/responses/${fileName}.json`, contentString);
+}
+
+function loadIndividual(fileName) {
+  const fileBuffer = fs.readFileSync(
+    `./tmp/responses/${fileName}.json`,
+    "utf-8"
+  );
+  const contentJson = JSON.parse(fileBuffer);
+  return contentJson;
+}
+
+function saveTickets(content, fileName) {
+  const contentString = JSON.stringify(content);
+  fs.writeFileSync(`./tmp/tickets/${fileName}.json`, contentString);
+  return fileName;
+}
+
 module.exports = {
   save,
-  saveIndividual,
   load,
+  saveIndividual,
+  loadIndividual,
+  saveTickets,
 };
